0de6363a4aff94ced293a4c303d46ff3798fe619
Edmund <edmund@li161-143.members.linode.com>
2010-11-14 09:15:17 +0000

    everything but host_method working

--- Makefile
+++ Makefile
@@ -2,9 +2,15 @@ CPPFLAGS=-Wall
 CXXFLAGS=`llvm-config --cppflags`
 LDFLAGS=`llvm-config  --ldflags --libs all` -lLLVM-2.7 -lstdc++
 
-all:	client.o host
+all:	client.s.bc host
+
+client.s.bc: client.s
+	llvm-as client.s
+
+client.s: client.cl
+	clang -emit-llvm -S client.cl
 
 host: host.o
 
 clean:
-	rm *.o host
+	rm *.o *.s *.s.bc host || true
--- host.cc
+++ host.cc
@@ -1,10 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+#include <llvm/DerivedTypes.h>
 #include <llvm/LLVMContext.h>
+#include <llvm/Linker.h>
 #include <llvm/Module.h>
+#include <llvm/System/Path.h>
 #include <llvm/ExecutionEngine/ExecutionEngine.h>
+#include <llvm/ExecutionEngine/GenericValue.h>
 using namespace llvm;
+using namespace llvm::sys;
+
+int host_method(int x){
+  return x * 3;
+}
 
 int main(){
-  Module *module = new Module("client code", getGlobalContext());
+
+  // Create a module that we can execute ..
+  Linker *linker = new Linker("host", "linked-client", getGlobalContext());
+  bool falsity = false;
+
+  // .. pull in the client code.
+  if (linker->LinkInFile(Path("client.s.bc"), falsity)) {
+    printf("llvm linker error: '%s'\n", linker->getLastError().c_str());
+    return 1;
+  }
+
+  // .. pull in the client-exposed host code.
+  FunctionType *ft = FunctionType::get(Type::getInt32Ty(getGlobalContext()), false);
+  Function::Create(ft, Function::ExternalLinkage, Twine("host_method"), linker->getModule());
+  // this is not working
+
+  // Finalize the module.
+  Module *module = linker->releaseModule();
+ 
+  // Make a new interpreter based on that module, and find the client_method.
   ExecutionEngine *engine = EngineBuilder(module).create();
+  Function *client_method = engine->FindFunctionNamed("client_method");
+  if (client_method == NULL) {
+    printf("Was not able to find 'client_method' function, aborting.\n");
+    return 1;
+  }
+
+  // Call the client_method with arguments.
+  std::vector<GenericValue> args(1);
+  args[0].UIntPairVal.first = 4;
+  engine->runFunction(client_method, args);
+
   return 0;
 }
